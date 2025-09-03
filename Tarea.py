efa40c6dd7366cc31dc5225f1371dbaf9f0eda52
Tomas Aguirre Sanz <t.aguirresanz@colegiocalasanz.edu.ar>
2025-09-03 13:21:16 -0300

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,13 @@
+pro={"Manzana":"5", "Pollo":"10", "Leche":"2"}
+lis=[]
+precio=0
+seguir="si"
+while seguir == "si":
+    sel=input("Que producto quiere:")
+    if sel in range pro:
+        lis.append(key)
+    elif sel=Comprar:
+        print lis
+        print precio
+    else:
+        print "error"
